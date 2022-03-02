ab6140dbc1dbc9c1704aadba05aa34aab1665607
NHK1905 <nguyenhuykhang195@gmail.com>
2022-03-02 14:25:37 +0700

    Update Feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
 import { FaqsContainer } from "../containers/faq";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 export default function Home() {
     return(
         <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>
+                        Unlimited Film, TV programmes and more.
+                    </Feature.Title>
+                    <Feature.Subtitle>
+                        Watch anywhere. Cancel at any time.
+                    </Feature.Subtitle>
+                </Feature>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address"/>
+                    <OptForm.Button>Get Started</OptForm.Button>
+                    <OptForm.Break/>
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your membership.
+                    </OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer/>
             <FooterContainer/>
